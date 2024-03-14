88b533bb23be9b3531ede63df39327efa5d4e10c
Yassin Musa <yassinmusa17@gmail.com>
2024-03-15 01:15:17 +0300

    Add project files.

---
+++ Carserver/App_Start/RouteConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Carserver
+{
+    public class RouteConfig
+    {
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "SendEmail",
+                url: "Home/SendEmail",
+                defaults: new { controller = "Home", action = "SendEmail" }
+            );
+
+            routes.MapRoute(
+                name: "About",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Contact",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Contact", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Services",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Services", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+        
+        }
+    }
+}
---
+++ Carserver/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Mvc;
+
+namespace Carserver.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        // GET: Services
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        // POST: Email
+        [HttpPost]
+        public ActionResult SendEmail(FormCollection form)
+        {
+            string name = form["w3lName"];
+            string senderEmail = form["w3lSender"];
+            string subject = form["w3lSubject"];
+            string message = form["w3lMessage"];
+
+            string fromEmail = ConfigurationManager.AppSettings["FromEmail"];
+            string password = ConfigurationManager.AppSettings["Password"];
+
+            string smtpServer = "smtp.gmail.com";
+            int port = 587;
+
+            using (var smtpClient = new SmtpClient(smtpServer, port))
+            {
+                smtpClient.EnableSsl = true;
+                smtpClient.Credentials = new NetworkCredential(fromEmail, password);
+
+                using (MailMessage mailMessage = new MailMessage(fromEmail, senderEmail))
+                {
+                    mailMessage.Subject = subject;
+                    mailMessage.Body = "Name: " + name + "\nEmail: " + senderEmail + "\n\nMessage:\n" + message;
+
+                    try
+                    {
+                        smtpClient.Send(mailMessage);
+                        ViewBag.Message = "Email sent successfully!";
+                    }
+                    catch (SmtpException ex)
+                    {
+                        ViewBag.Message = "Failed to send email: " + ex.Message;
+                    }
+                }
+            }
+
+            return RedirectToAction("Contact");
+        }
+    }
+}
